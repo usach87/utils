b095891ed93b7bad6a9784ca0dc3b3e56ebee982
usach87 <s.usachev87@gmail.com>
2022-05-23 11:52:24 +0300

    fix

---
+++ utils.ts
@@ -0,0 +1,48 @@
+type TRecursiveDictionaryType<T> = {[key: string]: T | TRecursiveDictionaryType<T>};
+
+function mergeByKeys<T>(obj: TRecursiveDictionaryType<T>, item: T, keys: Array<keyof T>): TRecursiveDictionaryType<T> {
+  const [key, ...restKeys] = keys;
+  const keyValue = String(item[key]);
+
+  if (!restKeys || !restKeys.length) {
+    return {
+      ...obj,
+      [keyValue]: item,
+    };
+  }
+
+  return {
+    ...obj,
+    [keyValue]: mergeByKeys((obj[keyValue] || {}) as TRecursiveDictionaryType<T>, item, restKeys),
+  };
+}
+
+export function groupBy<T>(list: T[], key: keyof T): Record<string, T[]> {
+  return list.reduce<Record<string, T[]>>((acc, item) => {
+    const preparedKey = String(item[key]);
+
+    if (!acc[preparedKey]) {
+      acc[preparedKey] = [];
+    }
+
+    acc[preparedKey].push(item);
+
+    return acc;
+  }, {});
+}
+
+/**
+  Версия v1
+  Источник: `Access-mapper`, `А360`, `БО ОП`, `ЛК`
+*/
+export function groupByKey<T>(list: T[], key: keyof T) {
+  return list.reduce((acc, item) => mergeByKeys(acc, item, [key]), {});
+}
+
+/**
+  Версия v1
+  Источник: `Access-mapper`, `А360`, `БО ОП`, `ЛК`
+*/
+export function groupByKeys<T>(list: T[], keys: Array<keyof T>) {
+  return list.reduce((acc, item) => mergeByKeys(acc, item, keys), {});
+}
